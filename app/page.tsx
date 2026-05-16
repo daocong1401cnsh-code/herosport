508dc25373a3e21bed0ede243fd6d8f7759dc5ca
daocong1401cnsh-code <daocong1401cnsh@gmail.com>
2026-05-16 19:29:13 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 export default function Home() {
-  const googleSheetUrl = "https://google.com"; // Thay bằng link Google Sheets thực tế của bạn nếu có
-
+const googleSheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSsTUQO9fUB3l8PrFnPDL0Y_IiFZ6RWqrRG2syD3jH49PVEKuj89fBuuU0TsKQqa4dia6m3t-VVfyfv/pubhtml";
   return (
     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', margin: 0, padding: 0 }}>
       <iframe
